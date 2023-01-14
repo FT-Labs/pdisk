f8e040b1f6020273fdb257f89522ff0d30b73dc0
Arda Atci <phystecharda@gmail.com>
2023-01-14 22:51:16 +0300

    pdisk v1.0

---
+++ part_guids.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+part_map = {
+    "Linux filesystem data" : "0FC63DAF-8483-4772-8E79-3D69D8477DE4",
+    "Raid partition" : "A19D880F-05FC-4D3B-A006-743F0F84911E",
+    "Root partition (x86)" : "44479540-F297-41B2-9AF7-D131D5F0458A",
+    "Root partition (x86-64)" : "4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709",
+    "/boot partition" : "BC13C2FF-59E6-4262-A352-B275FD6F7172",
+    "Swap partition" : "0657FD6D-A4AB-43C4-84E5-0933C84B4F4F",
+    "LVM partition" : "E6D6D379-F507-44C2-A23C-238F2A3DF928",
+    "/home partition" : "933AC7E1-2EB4-4F13-B844-0E14E2AEF915",
+    "LUKS partition" : "CA7D7CCB-63ED-4C53-861C-1742536059CC",
+    "MBR partition scheme" : "024DEE41-33E7-11D3-9D69-0008C781F39F",
+    "EFI system partition" : "C12A7328-F81F-11D2-BA4B-00A0C93EC93B"
+}
